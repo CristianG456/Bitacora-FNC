0f6397ea64ae15c31da9276c7dccd2905ec30a32
CristianG456 <cristiancronos123@gmail.com>
2026-05-22 09:15:26 -0500

    ajustes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,9 @@ php artisan migrate --force
 # Crear administrador a partir de variables de entorno
 php artisan app:create-admin
 
+# Ajustar permisos para que el servidor web (www-data) pueda escribir
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
 # Iniciar el proceso principal (php-fpm)
 exec php-fpm
